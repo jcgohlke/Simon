4d00fa275e711ce2ccbf730a5ca46feefef29720
Ben Gohlke <jcgohlke@me.com>
2020-05-14 21:43:17 -0400

    Initial Commit

---
+++ Simon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simon
+//
+//  Created by Joben Gohlke on 5/14/20.
+//  Copyright © 2020 Ben Gohlke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
